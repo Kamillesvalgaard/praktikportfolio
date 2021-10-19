98106232443a7556620bdb5faa98fe95ffaa56b0
kamilleSvalgaard <kami7277@stud.kea.dk>
2021-10-19 12:50:44 +0200

    gsap animationer

--- script.js
+++ script.js
@@ -3,8 +3,64 @@ gsap.from(".introduktion", {
   opacity: 0,
 });
 
-gsap.from("nav", {
-  x: -1000,
+gsap.from(".introduktion2", {
   duration: 2,
   opacity: 0,
+});
+
+gsap.from("nav", {
+  x: -1000,
+  duration: 1.5,
+  opacity: 0,
+});
+
+gsap.from(".fravenstre", {
+  x: -1000,
+  duration: 2.5,
+  opacity: 0,
+});
+
+gsap.from(".frahøjre", {
+  x: 1000,
+  duration: 2.5,
+  opacity: 0,
+});
+
+gsap.from(".fravenstre1", {
+  x: -1000,
+  duration: 2.5,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".frahøjre",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "bottom center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".frahøjre1", {
+  x: 1000,
+  duration: 2.5,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".fravenstre1",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "bottom center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".fravenstre2", {
+  x: -1000,
+  duration: 2.5,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".frahøjre1",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "bottom center",
+    end: "center 25%",
+  },
 });
